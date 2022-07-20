92514604d6298890b3ada9f2579e8ea45b6a7ded
Conan Goodwin <conan_goodwin@hotmail.com>
2022-07-20 16:37:53 -0300

    Requisitos 05 e 06

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import dataPlanet from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {dataPlanet.map((item) => <PlanetCard planetName={item.name} planetImage={item.image} />)}
       </div>
     );
   }
