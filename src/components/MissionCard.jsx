2550cc1e9ad360e0e12ecb49ada0ad7f8171713e
Conan Goodwin <conan_goodwin@hotmail.com>
2022-07-20 18:13:08 -0300

    Requisitos 07 e 08

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MissionCard extends React.Component {
+  render() {
+    const { data } = this.props;
+    const { name, year, country, destination} = data;
+
+    return (
+      <div data-testid="mission-card">
+        <p>{ name }</p>
+        <p>{ year }</p>
+        <p>{ country }</p>
+        <p>{ destination }</p>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    year: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+    destination: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MissionCard;
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import data from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {data.map((item) => <MissionCard key={ item.name } data={ item } />)}
       </div>
     );
   }
